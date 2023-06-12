c586172ea7717b499704056cce58c4364176451e
Myniuu <michalszczygiel96@gmail.com>
2023-06-12 20:20:14 +0200

    Finished additional tasks

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	if (a >= 0 || b >= 0) {
+  return a + b;
+  }
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(ar) {
+  if (ar.length === 0) {
+    return 0;
+  } else {
+    const arSum = ar.reduce((prev, curr) => prev + curr);
+    return arSum;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(ar) {
+  const multSum = ar.reduce((prev, curr) => prev * curr);
+  return multSum;
 };
 
-const power = function() {
-	
+const power = function(a ,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a == 0) return 1;
+  else return a * factorial(a - 1);
 };
 
 // Do not edit below this line
@@ -31,3 +40,5 @@ module.exports = {
   power,
   factorial
 };
+
+
