838e7b2e2b3776f8521a49ea33e772e74a3ba588
Myniuu <michalszczygiel96@gmail.com>
2023-06-13 21:27:52 +0200

    Done 2 more exercises (10, 11), fibonacci with large help

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
-
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
+const getTheTitles = function(ar) {
+  ar.forEach((el) => { 
+    const value = Object.values(el);
+    console.log(value.join(','))
+  });
 };
+
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const persons = [
+  {name: 'Janek', birthYear: 1920, birthDead: 2012},
+  {name: 'Zbysiu', birthYear: 1942, birthDead: 1999},
+  {name: 'Lolek', birthYear: 1992, birthDead: 2023},
+];
 
+const findTheOldest = function(ar) {
+  
 };
 
 // Do not edit below this line
